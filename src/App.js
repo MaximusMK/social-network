c0a4d869dbebd7b2d3d5c12abdc3dbee4d656939
maximusmk <intmaxmk@gamil.com>
2020-11-30 20:55:28 +0200

    L10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js';
-import Technologies from './Technologies.js';
+import Footer from './Footer';
+import Header from './Header';
+import Technologies from './Technologies';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+  return (
+    <div >
+     it-kamasutra.com 
+    </div>
+  );
+}
+  export default Header;
