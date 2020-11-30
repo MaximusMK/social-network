1fccc1f01d062b87130990a6268167cf7337d852
maximusmk <intmaxmk@gamil.com>
2020-11-30 22:22:05 +0200

    L12

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://pllsll.com/assets/tmp/storage/b6/be/79/b6b2a3834f4.jpg" alt="logo" />
+        </header>
+    )
+}
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+        <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_400,q_75,w_1366/v1/clients/daytonabeach/Beach_Aeriel_cx_af61e1fd-615c-4c57-8011-9689d196ae6b.jpg" alt=""/>
+      </div>
+      <div>ava + description</div>
+      <div>My posts</div>
+      <div>New post</div>
+      <div>Post1</div>
+      <div>Post2</div>
+    </div>
+    )
+}
+export default Profile;
