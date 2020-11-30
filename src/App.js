cbb37b5318ddda88fe61ccec06809eef40e41e4d
maximusmk <intmaxmk@gamil.com>
2020-11-30 22:18:06 +0200

    L12

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Profile from './Components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src="https://pllsll.com/assets/tmp/storage/b6/be/79/b6b2a3834f4.jpg" alt="logo" />
-      </header>
-      <nav className='nav'>
-        <div><a href="#">Profile</a></div>
-        <div><a href="#">Massages</a></div>
-        <div><a href="#">News</a></div>
-        <div><a href="#">Profile</a></div>
-        <div><a href="#">Settings</a></div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_400,q_75,w_1366/v1/clients/daytonabeach/Beach_Aeriel_cx_af61e1fd-615c-4c57-8011-9689d196ae6b.jpg" alt=""/>
-        </div>
-        <div>ava + description</div>
-        <div>My posts</div>
-        <div>New post</div>
-        <div>Post1</div>
-        <div>Post2</div>
-      </div>
+      <Header />
+      <Navbar/> 
+      <Profile/>
     </div>
   );
 }
