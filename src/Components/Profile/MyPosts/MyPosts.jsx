29f6c8264242036d4d9912b2177f2e72468d3aba
maximusmk <intmaxmk@gamil.com>
2020-12-01 20:47:25 +0200

    L15

---
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+  return (
+    <div>
+      MyPosts
+      <div>
+    <textarea></textarea>
+    <button>Button</button>
+
+    </div>
+      <div className={s.posts}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  )
+}
+export default MyPosts;
+
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
@@ -7,24 +8,10 @@ const Profile = () => {
       <div>
         <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_400,q_75,w_1366/v1/clients/daytonabeach/Beach_Aeriel_cx_af61e1fd-615c-4c57-8011-9689d196ae6b.jpg" alt="" />
       </div>
-
       <div>
         ava + description
       </div>
-      <div>
-        My posts
-        <div>
-          New post
-        </div>
-        <div className ={s.posts}>
-          <div className = {s.item}>
-            Post1
-          </div>
-          <div>
-            Post2
-          </div>
-        </div>
-      </div>
+      <MyPosts />
     </div>
   )
 }
