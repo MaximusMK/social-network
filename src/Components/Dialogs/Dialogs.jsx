aeab378742ab868adf84d616e1122310c57766ee
maximusmk <intmaxmk@gamil.com>
2020-12-15 22:57:00 +0200

    L39

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Mesage";
+import {addMessageActionCreator, updateNewMessageTextActionCreator} from "../../redux/state";
 
 const Dialogs = (props) => {
 
@@ -11,12 +12,14 @@ const Dialogs = (props) => {
     let newMessageElement = React.createRef();
 
     let addMessage = () => {
-        props.dispatch({type: 'ADD-MESSAGE'});
+        // props.dispatch({type: 'ADD-MESSAGE'});   //work
+        props.dispatch(addMessageActionCreator());
     }
     let onMessageChange = () => {
         let text = newMessageElement.current.value;
-        let action = {type: 'UPDATE-NEW-MESSAGE-TEXT', newMessageText: text};
-        props.dispatch(action);
+        // let action = {type: 'UPDATE-NEW-MESSAGE-TEXT', newMessageText: text};
+        // props.dispatch(action);
+        props.dispatch(updateNewMessageTextActionCreator(text));
     }
     return (
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,10 +1,15 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+
 let store = {
 
     _state: {
         profilePage: {
             posts: [
-                {id: 1, message: 'Hi, how are you?', likesCount: 15},
-                {id: 2, message: "it's my first post", likesCount: 25},
+                // {id: 1, message: 'Hi, how are you?', likesCount: 15},
+                // {id: 2, message: "it's my first post", likesCount: 25},
                 {id: 3, message: "Good", likesCount: 3},
             ],
             newPostText: 'Enter your post',
@@ -38,7 +43,7 @@ let store = {
 	},
 
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -48,11 +53,11 @@ let store = {
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'ADD-MESSAGE') {
+        } else if (action.type === ADD_MESSAGE) {
             let newMessage = {
                 id: 4,
                 message: this._state.dialogsPage.newMessageText,
@@ -61,12 +66,20 @@ let store = {
             this._state.dialogsPage.newMessageText = '';
             this._callSubscriber(this._state);
 
-        } else  if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
+        } else  if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
             this._state.dialogsPage.newMessageText = action.newMessageText;
             this._callSubscriber(this._state);
         }
-	}
+	},
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST});
+export const updateNewPostTextActionCreator = (text) =>
+    ({type: UPDATE_NEW_POST_TEXT, newText: text});
+
+export const addMessageActionCreator = () => ({type: ADD_MESSAGE});
+export const updateNewMessageTextActionCreator = (text) =>
+    ({type: UPDATE_NEW_MESSAGE_TEXT, newMessageText: text});
 
 export default store;
 window.store = store;
