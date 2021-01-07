ce38aa4b6bd337c7c5e29bf3b654f241b994c9f5
maximusmk <intmaxmk@gamil.com>
2021-01-07 15:42:07 +0200

    L63DAL,axios.create

---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "3744fa7c-b6f7-4f81-b99d-ddd3215a400d"
+    }
+});
+
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    }
+}
